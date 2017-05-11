a5f786cd1a221194fe068dcd2aeb22b7ad132b78
BoraALAP <alapbora@gmail.com>
2017-05-11 12:50:12 -0400

    taking of one of my works because of .....

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watchSass = require("gulp-watch-sass");
-var sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const livereload = require('gulp-livereload');
 
 gulp.task('sass', function() {
   gulp.src('scss/**/*.scss')
   	.pipe(sourcemaps.init())
-  .pipe(sass({style: 'expanded'}))
-  	.pipe(sourcemaps.write())
-  .pipe(gulp.dest('css'))
+  	.pipe(sass({style: 'compressed'}))
+  	.pipe(cleanCSS({compatibility: 'ie8'}))
+  	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))  	
+    .pipe(sourcemaps.write())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
 });
 
-gulp.task("sass:watch", () => {
-  gulp.watch("scss/*.scss", ["sass"])
+gulp.task("watch", () => {
+  livereload({ start: true });
+  gulp.watch("scss/**/*.scss", ["sass"]);
+
 })
 
